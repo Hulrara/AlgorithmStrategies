497445839292108253621ad6e09429e83d960873
던.김태훈 <thkim@lunasoft.co.kr>
2020-05-09 23:55:40 +0900

    데스크탑 정리본

---
+++ Boggle.kt
@@ -0,0 +1,11 @@
+class Boggle {
+    fun solve(){
+        val input = arrayOf(arrayOf("U","R","L","P","M"),
+            arrayOf("X","P","R","E","T"),
+            arrayOf("G","I","A","E","T"),
+            arrayOf("X","T","N","Z","Y"),
+            arrayOf("X","O","Q","R","S"))
+
+        val checkString = "PRETTY".toList()
+    }
+}
