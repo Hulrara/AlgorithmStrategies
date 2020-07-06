2caea12d95c9e976cb9e84f67558e726a1aee570
김태훈 <taehoon2852@gmail.com>
2020-07-06 19:58:10 +0900

    코딜리티 Lesson 4-1

--- FrogRiverOne.kt
+++ FrogRiverOne.kt
@@ -1,19 +1,43 @@
 import java.util.*
+import kotlin.collections.HashMap
 
 fun main() {
-    println(FrogRiverOne().solution(5, intArrayOf(1,3,1,4,2,3,5,4)))
+    println(FrogRiverOne().solution(5, intArrayOf(1, 2, 3, 4, 5)))
 }
+
 class FrogRiverOne {
     fun solution(X: Int, A: IntArray): Int {
-        var max = -1
-        A.mapIndexed { index, i -> Pair<Int, Int>(index, i) }
-            .groupBy { it.second }
-            .map { it.value.minBy { it.first}!! }
-            .sortedBy { it.second }
-            .filter { it.second <= X }
-            .forEach {
-                max = Math.max(max,it.first)
+
+        val visit = BooleanArray(X) { false }
+        var cnt = 0
+        for (i in A.indices) {
+            val a = A[i]
+            if (a > X || visit[a - 1]) {
+                continue
             }
-        return max
+            visit[a - 1] = true
+            cnt += 1
+            if (cnt == X) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    fun solution1(X: Int, A: IntArray): Int {
+
+        val visit = BooleanArray(X) { false }
+        var cnt = 0
+        A.forEachIndexed { index, i ->
+            if (i > X || visit[i - 1]){
+                return@forEachIndexed
+            }
+            visit[i - 1] = true
+            cnt += 1
+            if (cnt == X) {
+                return index
+            }
+        }
+        return -1
     }
 }
